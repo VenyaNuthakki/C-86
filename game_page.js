ac017e67224de46b3229a562bfbe05c4e8225b8e
VenyaNuthakki <71914805+VenyaNuthakki@users.noreply.github.com>
2020-11-25 16:57:47 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+var player1_name=localStorage.getItem("player1key");
+var player2_name=localStorage.getItem("player2key");
+var player1_score=0;
+var player2_score=0;
+document.getElementById("player1_name").innerHTML=player1_name+":";
+document.getElementById("player2_name").innerHTML=player2_name+":";
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+document.getElementById("player_question").innerHTML="question_turn:"+player1_name;
+document.getElementById("player_answer").innerHTML="answer_turn:"+player2_name;
